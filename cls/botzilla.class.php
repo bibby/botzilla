6dae3eae470620c9efba0f8ee08e5dc61f2d99af
bibby <bibby@botzilla.org>
2009-09-26 14:20:10 -0400

    added inclusion of Duration class

--- cls/botzilla.class.php
+++ cls/botzilla.class.php
@@ -50,6 +50,7 @@ require_once(BZ_CLS_PATH.'ziggi.class.php');
 //UTILS
 require_once(BZ_CLS_PATH."FileStorage.class.php");
 require_once(BZ_CLS_PATH."Color.class.php");
+require_once(BZ_CLS_PATH."Duration.class.php");
 
 // BZ_CLS_PATH is a good place for other class files
 #require_once(BZ_CLS_PATH."xmlparser.class.php");
